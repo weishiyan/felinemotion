34ccd838e9979eedee61db8c2cc5d591f0bfa1e3
Weishi Yan <wsyan@uw.edu>
2019-12-02 00:47:54 -0800

    updated sound component for classification

--- Felinemotion/sound_batch_csv.py
+++ Felinemotion/sound_batch_csv.py
@@ -9,9 +9,14 @@ from numpy import asarray
 from numpy import savetxt
 from os.path import join
 
-def save_csv(sound_folder,csv_folder):
+def save_csv(sound_folder):
     files_list = os.listdir(sound_folder)
+    temp = convert_mel_one(join(sound_folder,files_list[0]))
+    del files_list[0]
+    csv_file = temp.flatten()
     for i in files_list:
         converted = convert_mel_one(join(sound_folder,i))
-        i = i.replace('.wav', '.csv')
-        savetxt(join(csv_folder,i), converted)
+        flattened = converted.flatten()
+        csv_file = np.vstack((csv_file,flattened))
+    
+    savetxt(sound_folder+'/raw.csv',csv_file,delimiter=",")
