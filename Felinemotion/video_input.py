ec944cbcd50d99c0e4c648515592783871e19225
ng-git <minh4829@gmail.com>
2019-12-03 10:28:26 -0800

    pep8 fixed for video_input.py

--- Felinemotion/video_input.py
+++ Felinemotion/video_input.py
@@ -1,6 +1,5 @@
 import os
 import cv2
-import numpy as np
 import moviepy.editor as mp
 
 """ This script receive a mp4 file in the same directory as input
@@ -12,8 +11,8 @@ def video_input(name):
 
     # Extract the audio into wav file
     name = name - ".mp4"
-    audioClip = mp.AudioFileClip(name + ".mp4")
-    audioClip.write_audiofile(name + ".wav")
+    audio = mp.AudioFileClip(name + ".mp4")
+    audio.write_audiofile(name + ".wav")
 
     # Create a VideoCapture object
     cam = cv2.VideoCapture(name + ".mp4")
@@ -28,27 +27,27 @@ def video_input(name):
         print('Error: Creating directory of frames')
 
     # Check if camera opened successfully
-    if (cam.isOpened() == False):
+    if cam.isOpened() is False:
         print("Unable to read camera feed")
 
     # frame
-    currentframe = 0
-    while (True):
+    current_frame = 0
+    while True:
 
         # reading from frame
         ret, frame = cam.read()
 
         if ret:
             # if video is still left continue creating images
-            frameName = './' + name + '/frame' + str(currentframe) + '.jpg'
+            frame_name = './' + name + '/frame' + str(current_frame) + '.jpg'
             # print('Creating...' + frameName)
 
             # writing the extracted images
-            cv2.imwrite(frameName, frame)
+            cv2.imwrite(frame_name, frame)
 
             # increasing counter so that it will
             # show how many frames are created
-            currentframe += 1
+            current_frame += 1
         else:
             break
 
