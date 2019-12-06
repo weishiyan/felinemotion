53a49236cfda3441b8be5d0b18f12d0b5bceab2c
yueguo-50 <yguo50@uw.edu>
2019-12-05 21:41:01 -0800

    csv pylint

---
+++ Felinemotion/svm.py
@@ -0,0 +1,58 @@
+'''Return the cat emotion
+   Imput: tested image and audio data
+   Output: cat emotion
+   '''
+
+import os
+import pandas as pd
+from sklearn.svm import LinearSVC
+
+def csv_merge(image, audio):
+    '''
+	Import test image and audio datasets and merge by catID
+	Imput: image and audio datasets in csv
+	Output: one dataset with both image and audio data
+        '''
+# check whether the dataset exist in the current work directory
+    if not os.path.exists(image):
+        raise ValueError("Invalid input file.")
+    if not os.path.exists(audio):
+        raise ValueError("Invalid input file.")
+
+    image = pd.read_csv(image)
+    audio = pd.read_csv(audio)
+
+    data = audio.merge(image, on='catID')
+    data = data.iloc[:, 1:]
+
+    data.to_csv('data_test.csv')
+
+def classification(term):
+    '''
+	Return the cat emotion
+	Imput: data_test.csv with both tested image and audio data
+	Output: emotion in string
+        '''
+# emotion should be included in audio file
+# import the training dataset
+    image = pd.read_csv('image.csv')
+    audio = pd.read_csv('audio.csv')
+
+    data = audio.merge(image, on='catID')
+
+# build the model
+    x_value = data.iloc[:, 2:]
+    y_value = data.emotion
+
+    clf = LinearSVC(random_state=0, tol=1e-5)
+    clf.fit(x_value, y_value)
+
+# read test csv into dataframe
+    if not os.path.exists(term):
+        raise ValueError("Invalid input file.")
+
+    test = pd.read_csv(term)
+    test = test.iolc[:, 1:]
+
+    y_pred = clf.predict(test)
+    return(y_pred)
