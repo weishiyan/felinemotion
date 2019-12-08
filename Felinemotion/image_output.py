c915f46bbd52195aae9182eea4e476db96a1e8ec
j555010 <56062628+j555010@users.noreply.github.com>
2019-12-08 15:21:14 -0800

    Update image_output.py

--- Felinemotion/image_output.py
+++ Felinemotion/image_output.py
@@ -27,7 +27,7 @@ def image_output(directory, image):
         sum_img = sum(img2[j:100 + j])
         avg_img = sum_img / 100
         i2 = i + 1
-	    num_data.append(i2)
+	num_data.append(i2)
         img_data.append(avg_img)
     df_img = pd.DataFrame({'CatID':num_data,'useInput':img_data})
     df_img = df_img.transpose()
