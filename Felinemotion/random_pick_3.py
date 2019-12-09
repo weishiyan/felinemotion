f0bbf8eda5061207397ad7cdd3ae1ca385652e60
j555010 <56062628+j555010@users.noreply.github.com>
2019-12-08 16:54:13 -0800

    Update random_pick_3.py

--- Felinemotion/random_pick_3.py
+++ Felinemotion/random_pick_3.py
@@ -7,19 +7,21 @@ Created on Sat Dec  7 21:00:55 2019
 import os
 import random
 import cv2
-def pick_three(path2string, outputstring):
+def pick_three(path2string):
 	"""
 	test
 	"""
 	# dir = "userData/"
 	path2 = path2string
-	output = outputstring
+	#output = outputstring
 	files_out = []
 	# r=root, d=directories, f = files
 	for root, directories, files in os.walk(path2):
 		for file in files:
 			files_out.append(os.path.join(root, file))
 	rand = random.sample(files_out, 3)
+	dirName = 'image_cat3'
+	os.mkdir(dirName)
 	pick1 = rand[1]
 	pick2 = rand[2]
 	pick3 = rand[0]
@@ -29,6 +31,6 @@ def pick_three(path2string, outputstring):
 	base1 = 'user1.jpg'
 	base2 = 'user2.jpg'
 	base3 = 'user3.jpg'
-	cv2.imwrite(os.path.join(output, base1), pick1_1)
-	cv2.imwrite(os.path.join(output, base2), pick1_2)
-	cv2.imwrite(os.path.join(output, base3), pick1_3)
+	cv2.imwrite(os.path.join('image_cat3/', base1), pick1_1)
+	cv2.imwrite(os.path.join('image_cat3/', base2), pick1_2)
+	cv2.imwrite(os.path.join('image_cat3/', base3), pick1_3)
