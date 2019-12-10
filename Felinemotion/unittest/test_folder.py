79141534dec626d5ceceeceaa2fb53151cc30495
j555010 <56062628+j555010@users.noreply.github.com>
2019-12-09 23:44:47 -0800

    Add files via upload

--- Felinemotion/unittest/test_folder.py
+++ Felinemotion/unittest/test_folder.py
@@ -1,19 +1,20 @@
-#!/usr/bin/env python
-# coding: utf-8
-
+"""
+unit test for image_analysis
+"""
 import unittest
-
-import image_analysis
 import os
+import image_analysis
 class UnitTests(unittest.TestCase):
+    """
+    unit test
+    """
     def test_folder(self):
-        '''
+        """
         Check if the model creates a file for saving cat face images
-        '''
+        """
         image_analysis.cat_detect('userData/frames/', 'haarcascade_frontalcatface.xml')
         created_folder = 'userData/catFaces/'
         self.assertTrue(os.path.exists(created_folder))
 
 suite = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
 _ = unittest.TextTestRunner().run(suite)
-
