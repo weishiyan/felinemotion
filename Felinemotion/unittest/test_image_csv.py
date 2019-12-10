dff4a29578f4dde900b4aaa47a9837eb3d6fbcb7
j555010 <56062628+j555010@users.noreply.github.com>
2019-12-09 23:49:19 -0800

    Add files via upload

--- Felinemotion/unittest/test_image_csv.py
+++ Felinemotion/unittest/test_image_csv.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python
-# coding: utf-8
-
+"""
+unit test for module image_output
+"""
+import os
 import unittest
 import image_output
-import os
-
-
 class UnitTests(unittest.TestCase):
+    """
+    unit test
+    """
     def test_data(self):
         '''
         Check if the output csv file exist
         '''
         image_output.image_output('userData/', 'user1.jpg')
         self.assertTrue(os.path. isfile('userData/selected_image.csv'))
-    
+
 suite = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
 _ = unittest.TextTestRunner().run(suite)
-
