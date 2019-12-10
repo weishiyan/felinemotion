976febeca7608c48edf19565be229c3cee67d643
j555010 <56062628+j555010@users.noreply.github.com>
2019-12-09 22:35:20 -0800

    Add files via upload

---
+++ Felinemotion/unittest/test_random.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import unittest
+
+
+# In[ ]:
+
+
+import random_pick_3
+
+
+# In[ ]:
+
+
+class UnitTests(unittest.TestCase):
+
+
+# In[ ]:
+
+
+def test_image(self):
+
+
+# In[ ]:
+
+
+'''
+Check if the output file is jpg
+'''
+
+
+# In[ ]:
+
+
+self.assertTrue(os.path. isfile('userData/user1.jpg'))
+
+
+# In[ ]:
+
+
+suite = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
+
+
+# In[ ]:
+
+
+_ = unittest.TestTestRunner().run(suite)
+
